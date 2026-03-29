70ced0f829bed80d265c05a1c809dc65620ffe64
Giovanni <giovanniw30@hotmail.com>
2026-03-29 14:22:37 -0300

    feat: adiciona estrutura inicial do frontend

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('Frontend rodando');
-});
-
-server.listen(4200, () => {
-    console.log('Frontend rodando na porta 4200');
-});
